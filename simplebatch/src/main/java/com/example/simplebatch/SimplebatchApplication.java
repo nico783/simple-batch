137602a85236d6fb19679c619583795881c829c0
Nicolas Benizri <nicolasbenizri@MacBook-Pro-de-Nicolas.local>
2017-07-15 10:28:48 +0200

    initialisation projet

---
+++ simplebatch/src/main/java/com/example/simplebatch/SimplebatchApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebatchApplication.class, args);
+	}
+}
