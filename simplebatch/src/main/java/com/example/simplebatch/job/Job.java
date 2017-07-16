442b22992927604fc314f080d0e99f9d77380918
Nicolas Benizri <nicolasbenizri@MacBook-Pro-de-Nicolas.local>
2017-07-16 18:48:00 +0200

    [clean] execution process dans la classe job

--- simplebatch/src/main/java/com/example/simplebatch/job/Job.java
+++ simplebatch/src/main/java/com/example/simplebatch/job/Job.java
@@ -9,10 +9,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.simplebatch.reader.entity.ReadEntity;
 import com.example.simplebatch.reader.service.ReaderService;
+import com.example.simplebatch.writer.entity.WriteEntity;
 import com.example.simplebatch.writer.service.WriterService;
 
 @Component
 public class Job {
+	
+	@Value(value = "${batch.size}")
+	private String stringBatchSize;
 	
 	@Autowired
 	private ReaderService readerService;
@@ -22,8 +26,27 @@ public class Job {
 
 	@Transactional
 	public void execute() {
+		int batchSize = Integer.parseInt(stringBatchSize);
+		
+		// Récupération de l'ensemble des données dans un flux.
 		Stream<ReadEntity> readEntities = readerService.findAllEntities();
-		writerService.write(readEntities);
+		
+		// Transformation des données puis écriture en bdd.
+		readEntities.map(x -> process(x)).forEach(t -> writerService.write(t, batchSize));
+	}
+	
+	/**
+	 * Applique un traitement sur les données lues.
+	 * 
+	 * @param Entité lue
+	 * @return Entité à écrire
+	 */
+	private WriteEntity process(ReadEntity x) {
+		WriteEntity writeEntity = new WriteEntity();
+		writeEntity.setFirstName(x.getPrenom());
+		writeEntity.setId(x.getId());
+		writeEntity.setLastName(x.getNom());
+		return writeEntity;
 	}
 
 }
--- simplebatch/src/main/java/com/example/simplebatch/writer/service/WriterService.java
+++ simplebatch/src/main/java/com/example/simplebatch/writer/service/WriterService.java
@@ -1,59 +1,28 @@
 package com.example.simplebatch.writer.service;
 
-import java.util.stream.Stream;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.simplebatch.reader.entity.ReadEntity;
 import com.example.simplebatch.writer.entity.WriteEntity;
 import com.example.simplebatch.writer.repository.WriterRepository;
 
 @Service
 public class WriterService {
 	
-	@Value(value = "${batch.size}")
-	private String stringBatchSize;
-
 	@Autowired
 	private WriterRepository writerRepository;
 	
 	private long compteur = 0;
 
 	@Transactional
-	public void write(Stream<ReadEntity> readEntities) {
-		int batchSize = Integer.parseInt(stringBatchSize);
-		readEntities.map(x -> process(x)).forEach(t -> writeAndClear(t, batchSize));
-	}
-	
-	private synchronized void writeAndClear(WriteEntity entity, int batchSize) {
-		// WriteEntity persistentEntity =
+	public synchronized void write(WriteEntity entity, int batchSize) {
 		writerRepository.save(entity);
 		compteur++;
 		
 		if (compteur % batchSize == 0) {
 			writerRepository.flushAndClear();
-			System.out.println("Nombre de lignes insérées : " + compteur);
 		}
-
-		// System.out.println(writerRepository.contains(persistentEntity));
-		// System.out.println(writerRepository.contains(entity));
 	}
 	
-	/**
-	 * Applique un traitement sur les données lues.
-	 * 
-	 * @param x
-	 * @return
-	 */
-	private WriteEntity process(ReadEntity x) {
-		WriteEntity writeEntity = new WriteEntity();
-		writeEntity.setFirstName(x.getPrenom());
-		writeEntity.setId(x.getId());
-		writeEntity.setLastName(x.getNom());
-		return writeEntity;
-	}
-
 }
